26bb35ee6499cdd6c34e9dee941bba675a0be21c
gbarton <garrett.barton@gmail.com>
2014-05-19 18:52:51 -0400

    actually got webservice working, forgot @Produces

--- ssc-core/src/main/java/com/eqt/ssc/web/rest/AccountResource.java
+++ ssc-core/src/main/java/com/eqt/ssc/web/rest/AccountResource.java
@@ -1,9 +1,14 @@
 package com.eqt.ssc.web.rest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
@@ -11,6 +16,7 @@ import javax.ws.rs.core.Response.Status;
 import com.eqt.ssc.accounts.AccountManager;
 import com.eqt.ssc.accounts.AccountManagerFactory;
 import com.eqt.ssc.model.SSCAccount;
+import com.eqt.ssc.model.Token;
 
 @Path("/")
 public class AccountResource {
@@ -18,9 +24,24 @@ public class AccountResource {
 	//our 'dao'
 	AccountManager man = AccountManagerFactory.getInstance();
 	
+	public static class ManagedAccounts {
+		public List<String> accounts = new ArrayList<String>();
+	}
+	
+	@GET
+	@Path("managedAccounts")
+	@Produces(MediaType.APPLICATION_JSON)
+	public ManagedAccounts getManagedAccounts() {
+		ManagedAccounts m = new ManagedAccounts();
+		List<Token> accounts = man.getAccounts();
+		for(Token t : accounts)
+			m.accounts.add(t.getAccountId());
+		return m;
+	}
+
 	@PUT
-	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
 	@Path("addAccount")
 	public Response putAccount(SSCAccount account) {
 		if(account != null) {
