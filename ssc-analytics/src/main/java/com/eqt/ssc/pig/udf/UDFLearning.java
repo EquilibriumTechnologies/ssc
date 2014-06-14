62414302d75102400ee5766f9edccdc35898898b
gman <garrett.barton@gmail.com>
2014-06-14 15:05:17 -0400

    modified pom to build analytics project

--- ssc-analytics/src/main/java/com/eqt/ssc/pig/udf/UDFLearning.java
+++ ssc-analytics/src/main/java/com/eqt/ssc/pig/udf/UDFLearning.java
@@ -6,30 +6,46 @@ import org.apache.pig.EvalFunc;
 import org.apache.pig.data.DataBag;
 import org.apache.pig.data.DataType;
 import org.apache.pig.data.Tuple;
+import org.apache.pig.data.TupleFactory;
 import org.apache.pig.impl.logicalLayer.schema.Schema;
 
+import com.amazonaws.services.ec2.model.DescribeRegionsResult;
+import com.eqt.ssc.model.SSCRecord;
+import com.eqt.ssc.serde.RecordBuilder;
+
 //reads in a json string representing the DescribeRegionResult
-public class UDFLearning extends EvalFunc<DataBag> {
+public class UDFLearning extends EvalFunc<Tuple> {
+	TupleFactory tupleF = TupleFactory.getInstance();
 
 	@Override
-	public DataBag exec(Tuple tuple) throws IOException {
+	public Tuple exec(Tuple tuple) throws IOException {
+		String json = tuple.toString();
+		System.out.println("JSON: " + json);
+		json = json.substring(1,json.length()-1);
+		System.out.println("JSON: " + json);
+		SSCRecord record = RecordBuilder.read(json);
+		tupleF.newTuple(record.value);
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	public Schema outputSchema(Schema input) {
 		try {
-			Schema.FieldSchema tokenFs = new Schema.FieldSchema("token", DataType.CHARARRAY);
-			Schema tupleSchema = new Schema(tokenFs);
-
-			Schema.FieldSchema tupleFs;
-			tupleFs = new Schema.FieldSchema("tuple_of_tokens", tupleSchema, DataType.TUPLE);
-
-			Schema bagSchema = new Schema(tupleFs);
-			bagSchema.setTwoLevelAccessRequired(true);
-			Schema.FieldSchema bagFs = new Schema.FieldSchema("bag_of_tokenTuples", bagSchema, DataType.BAG);
-
-			return new Schema(bagFs);
+//			Schema.FieldSchema describeRegionResultTuple = new Schema.FieldSchema("RegionResult", t)
+			Schema.FieldSchema s  = DataType.determineFieldSchema(new DescribeRegionsResult());
+			
+			return new Schema(s);
+//			Schema.FieldSchema tokenFs = new Schema.FieldSchema("token", DataType.CHARARRAY);
+//			Schema tupleSchema = new Schema(tokenFs);
+//
+//			Schema.FieldSchema tupleFs;
+//			tupleFs = new Schema.FieldSchema("tuple_of_tokens", tupleSchema, DataType.TUPLE);
+//
+//			Schema bagSchema = new Schema(tupleFs);
+//			bagSchema.setTwoLevelAccessRequired(true);
+//			Schema.FieldSchema bagFs = new Schema.FieldSchema("bag_of_tokenTuples", bagSchema, DataType.BAG);
+//
+//			return new Schema(bagFs);
 
 		} catch (Exception e) {
 			return null;
