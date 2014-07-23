db053fb98067a3e930d0e5a4b155c227aef64ab2
gbarton <garrett.barton@gmail.com>
2014-07-22 21:43:17 -0400

    fixed stop script

--- ssc-dist/src/main/scripts/bin/ssc.sh
+++ ssc-dist/src/main/scripts/bin/ssc.sh
@@ -63,7 +63,7 @@ elif [[ "$1" == "stop" ]]; then
  echo "stopping";
  #check for existence first
  if [ -f $PID_FILE ]; then
-  if kill -0 `cat $PID_FILE` > /dev/null 2>&1; then
+  if kill `cat $PID_FILE` > /dev/null 2>&1; then
    echo "stopped process";
   else
    echo "could not stop the process $(cat $PID_FILE)";
